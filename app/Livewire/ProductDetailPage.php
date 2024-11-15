e5db83e1871a4bef24ec93a84dd2f8ffb75d49a3
Roberto <roberto.britz@hotmail.com>
2024-11-15 14:03:04 +0000

    Making the product detail add to cart and working in add or remove quantity

--- app/Livewire/ProductDetailPage.php
+++ app/Livewire/ProductDetailPage.php
@@ -2,7 +2,10 @@
 
 namespace App\Livewire;
 
+use App\Helpers\CartManagement;
+use App\Livewire\Partials\Navbar;
 use App\Models\Product;
+use Jantinnerezo\LivewireAlert\LivewireAlert;
 use Livewire\Attributes\Title;
 use Livewire\Component;
 
@@ -10,11 +13,38 @@ use Livewire\Component;
 #[Title('Product Detail')]
 class ProductDetailPage extends Component
 {
+    use LivewireAlert;
     public $slug;
+    public $quantity = 1;
 
     public function mount($slug){
         $this->slug = $slug;
     }
+
+    public function increaseQty(){
+        $this->quantity++;
+    }
+
+    public function decreaseQty(){
+        if($this->quantity>1){
+        $this->quantity--;
+        }
+    }
+
+    // add product to cart method
+    public function addToCart($product_id){
+        $total_count = CartManagement::addItemToCart($product_id);
+
+        $this->dispatch('update-cart-count', total_count: $total_count)->to(Navbar::class);
+
+        $this->alert('success', 'Produdo adicionado no carrinho!',[
+            'position' => 'bottom-end',
+            'timer' => 3000,
+            'toast' => true,
+        ]);
+    }
+
+
     public function render()
     {
         return view('livewire.product-detail-page',[
