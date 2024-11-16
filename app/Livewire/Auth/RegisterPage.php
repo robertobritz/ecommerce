920f27bbb77c4dc13a5d3b51baa3c0ca0ec16ed9
Roberto <roberto.britz@hotmail.com>
2024-11-16 14:37:39 +0000

    Creating User from form

--- app/Livewire/Auth/RegisterPage.php
+++ app/Livewire/Auth/RegisterPage.php
@@ -2,10 +2,43 @@
 
 namespace App\Livewire\Auth;
 
+use App\Models\User;
+use Illuminate\Container\Attributes\Auth;
+use Illuminate\Support\Facades\Hash;
+use Livewire\Attributes\Title;
 use Livewire\Component;
+
+
+#[Title('Registro')]
 
 class RegisterPage extends Component
 {
+    public $name;
+    public $email;
+    public $password;
+
+    // registerr user
+    public function save(){
+        $this->validate([
+            'name' => 'required|max:255',
+            'email' => 'required|email|unique:users|max:255',
+            'password' => 'required|min:6|max:255',
+        ]);
+
+        //save database
+        $user = User::create([
+            'name' => $this->name,
+            'email' => $this->email,
+            'password' =>  Hash::make($this->password),
+        ]);
+
+        // login user
+        //Auth::login($user);
+        auth()->login($user);
+
+        // redirect to home page
+        return redirect()->intended();       //to('/');
+    }
     public function render()
     {
         return view('livewire.auth.register-page');
