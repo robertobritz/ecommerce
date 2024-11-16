2a64a867bf48e486d478c8aa699a2279095e17fe
Roberto <roberto.britz@hotmail.com>
2024-11-16 15:23:08 +0000

    Making singup and login forms works

--- resources/views/livewire/cart-page.blade.php
+++ resources/views/livewire/cart-page.blade.php
@@ -65,7 +65,7 @@
               <span class="font-semibold">{{ Number::currency($grand_total, 'BRL') }}</span>
             </div>
             @if ($cart_items)
-                <button class="w-full px-4 py-2 mt-4 text-white bg-blue-500 rounded-lg">Checkout</button>
+                <a href="/checkout" class="block w-full px-4 py-2 mt-4 text-center text-white bg-blue-500 rounded-lg ">Checkout</a>
             @endif
           </div>
         </div>
