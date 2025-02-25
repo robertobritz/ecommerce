a705ceed9e34f6d67d48fa4628c21f61c9488211
Roberto <roberto.britz@hotmail.com>
2025-02-25 21:25:27 +0000

    Testing feature_1

--- resources/views/mail/orders/placed.blade.php
+++ resources/views/mail/orders/placed.blade.php
@@ -1,13 +1,13 @@
 <x-mail::message>
-# Ordem realizada com sucesso!
+    # Ordem realizada com sucesso!
 
-Obrigado por sua compra!
-O número do seu pedido é: {{ $order->id }}
+    Obrigado por sua compra!!
+    O número do seu pedido é: {{ $order->id }}
 
-<x-mail::button :url="$url">
-Ver o pedido no meu perfil
-</x-mail::button>
+    <x-mail::button :url="$url">
+        Ver o pedido no meu perfil
+    </x-mail::button>
 
-Obrigado! <br>
-{{ config('app.name') }}
+    Obrigado! <br>
+    {{ config('app.name') }}
 </x-mail::message>
